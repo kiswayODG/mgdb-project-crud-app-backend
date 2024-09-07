0dac0a63fe82932d9299e8a7d857ce9858d318d5
kiswayODG <ulrichouedraogo500@gmail.com>
2024-09-07 10:42:04 +0000

    x

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+dotenv.config();
 
 // routes
 app.use("/api/etudiant", etudiantRoute);
@@ -19,9 +20,7 @@ app.get("/", (req, res) => {
 
 
 mongoose
-  .connect(
-    "mongodb://localhost:27017/"
-  )
+.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to database!");
     app.listen(3000, () => {
